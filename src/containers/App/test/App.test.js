88534dc797d69d3986ed84918ec19d03084cbca8
Joel O'Connor <joconno07@gmail.com>
2018-09-06 12:05:13 -0600

    Initial boiler plate setup

---
+++ src/containers/App/test/App.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../';
+
+describe('App Tests', () => {
+  it('should render App with the correct props', () => {
+    expect(true).toEqual(true)
+  });
+})
