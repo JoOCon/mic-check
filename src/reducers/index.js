71aca8d9212f7a1d83e3590d12823e8ba081aecf
Joel O'Connor <joconno07@gmail.com>
2018-09-10 09:12:54 -0600

    Added reducers to root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,12 @@
 import { combineReducers } from 'redux';
 import { userReducer } from '../reducers/userReducer';
+import { activeUserReducer } from '../reducers/activeUserReducer';
+import { rentalReducer } from '../reducers/rentalReducer';
+import { quoteReducer } from '../reducers/quoteReducer';
 
 export const rootReducer = combineReducers({
-  users: userReducer
+  users: userReducer,
+  activeUser: activeUserReducer,
+  rentals: rentalReducer,
+  quote: quoteReducer
 });
