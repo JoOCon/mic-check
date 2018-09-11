ad6e218a626a7a634581682f817cf45e833fb210
Joel O'Connor <joconno07@gmail.com>
2018-09-10 23:30:27 -0600

    Update to handle local storage user data

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,14 @@
 export const userReducer = (state = [], action) => {
+  let addedNewUser;
+  let stringedUsers;
   switch (action.type) {
     case 'ADD_USER':
-      return [action.user, ...state];
+      addedNewUser = [action.user, ...state];
+      stringedUsers = JSON.stringify(addedNewUser);
+      localStorage.setItem('AllUsers', stringedUsers);
+      return addedNewUser;
+    case 'GET_LOCAL_STORAGE_USERS':
+      return action.users || state;
     default:
       return state;
   }
