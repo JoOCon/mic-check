a19af9af898ebbf429f32006224949ffa28fa59b
Joel O'Connor <joconno07@gmail.com>
2018-09-10 23:31:04 -0600

    Update to Thunk to sucessfuly fetch a quote

--- src/thunk/getDeliveryQuote.js
+++ src/thunk/getDeliveryQuote.js
@@ -3,50 +3,30 @@ import { addQuote } from '../actions';
 import PropTypes from 'prop-types';
 
 export const getDeliveryQuote = (fromAddress, toAddress) => {
+  let addresses = {
+    pickup_address: "20 McAllister St, San Francisco, CA",
+    dropoff_address: "101 Market St, San Francisco, CA"
+  };
+
+  var deliveryBody = [];
+  for (var property in addresses) {
+    var encodedKey = encodeURIComponent(property);
+    var encodedValue = encodeURIComponent(addresses[property]);
+    deliveryBody.push(encodedKey + "=" + encodedValue);
+  }
+  deliveryBody = deliveryBody.join("&");
+
   return async (dispatch) => {
     const url = `https://api.postmates.com/v1/customers/${customer_id}/delivery_quotes`;
     const response = await fetch(url, {
       headers: {
-        [deliveryApiKey]:'',
-        'Content-Type': 'application/json'
+        'Authorization': "Basic " + btoa(deliveryApiKey + ":" + ""),
+        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
       },
-      method: 'Post',
-      body: JSON.stringify({
-        pickup_address: '20 McAllister St, San Francisco, CA',
-        dropoff_address: '101 Market St, San Francisco, CA'
-      })
+      method: 'POST',
+      body: deliveryBody
     });
     const quote = await response.json();
-    console.log(quote);
     dispatch(addQuote(quote));
   };
 };
-
-getDeliveryQuote.propTypes = {
-  fromAddress: PropTypes.string,
-  toAddress: PropTypes.string
-};
-
-// var form = new FormData();
-// form.append("pickup_address", "20 McAllister St, San Francisco, CA");
-// form.append("dropoff_address", "101 Market St, San Francisco, CA");
-
-// var settings = {
-//   "async": true,
-//   "crossDomain": true,
-//   "url": "https://api.postmates.com/v1/customers/cus_LsvzF5CZMxpsDk/delivery_quotes",
-//   "method": "POST",
-//   "headers": {
-//     "Authorization": "Basic OWE4MTk4Y2QtZGVlZi00ZGIyLTk1NDAtM2ExMGJjYWE1Mzg0Og==",
-//     "Cache-Control": "no-cache",
-//     "Postman-Token": "5e01eac6-0ce1-499c-8a03-ed4d7ebf6222"
-//   },
-//   "processData": false,
-//   "contentType": false,
-//   "mimeType": "multipart/form-data",
-//   "data": form
-// };
-
-// $.ajax(settings).done(function (response) {
-//   console.log(response);
-// });
