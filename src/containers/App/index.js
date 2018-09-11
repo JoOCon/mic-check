fcabe2f8c327ab06e10f9664fb4cd91d6cc7bda8
Joel O'Connor <joconno07@gmail.com>
2018-09-10 23:21:41 -0600

    Added functionality to retrive local storage users array and dispatch it to the store

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import { Header } from '../../components/Header';
 import { connect } from 'react-redux';
 import { Route, withRouter} from 'react-router-dom';
+import { getLocalStorageUsers } from '../../actions';
 import RentalsContainer from '../RentalsContainer';
 import SignUp from '../SignUp';
 import Login from '../Login';
+import RentalForm from '../RentalForm';
+import PropTypes from 'prop-types';
 import './styles.css';
 
 export class App extends Component {
+  componentDidMount() {
+    const users = JSON.parse(localStorage.getItem('AllUsers'));
+    this.props.getLocalStorageUsers(users);
+  }
+
   render() {
     return (
       <div className="App">
@@ -15,9 +23,18 @@ export class App extends Component {
         <Route path='/' component={RentalsContainer} />
         <Route path='/' component={Login} />
         <Route path='/' component={SignUp} />
+        <Route path='/' component={RentalForm} />
       </div>
     );
   }
 }
 
-export default withRouter(connect()(App));
+export const mapDispatchToProps = (dispatch) => ({
+  getLocalStorageUsers: (users) => dispatch(getLocalStorageUsers(users))
+});
+
+App.propTypes = {
+  getLocalStorageUsers: PropTypes.func
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
