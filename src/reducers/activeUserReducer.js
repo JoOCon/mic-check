ee6e412f820bff708f609cabfe9dab1e35194c51
Joel O'Connor <joconno07@gmail.com>
2018-09-12 09:59:43 -0600

    Added case to active user reducer to handle logging out user

--- src/reducers/activeUserReducer.js
+++ src/reducers/activeUserReducer.js
@@ -2,6 +2,8 @@ export const activeUserReducer = (state = {}, action) => {
   switch (action.type) {
     case 'SET_ACTIVE_USER':
       return action.user;
+    case 'LOGOUT_ACTIVE_USER':
+      return {};
     default:
       return state;
   }
