bea18b9040e484e818d0aa1a88403a439ee00e33
Joel O'Connor <joconno07@gmail.com>
2018-09-12 15:26:16 -0600

    Built out localStorage functionality for rentals

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import { Header } from '../../components/Header';
 import { connect } from 'react-redux';
 import { Route, withRouter} from 'react-router-dom';
-import { getLocalStorageUsers } from '../../actions';
+import { getLocalStorageUsers, getLocalStorageRentals } from '../../actions';
 import RentalsContainer from '../RentalsContainer';
+import UserAccountDisplay from '../UserAccountDisplay';
 import SignUp from '../SignUp';
 import Login from '../Login';
 import RentalForm from '../RentalForm';
@@ -12,29 +13,42 @@ import './styles.css';
 
 export class App extends Component {
   componentDidMount() {
+    const { getLocalStorageUsers, getLocalStorageRentals } = this.props;
     const users = JSON.parse(localStorage.getItem('AllUsers'));
-    this.props.getLocalStorageUsers(users);
+    const rentals = JSON.parse(localStorage.getItem('AllRentals'));
+    getLocalStorageUsers(users);
+    getLocalStorageRentals(rentals);
   }
 
   render() {
+    const { userName } = this.props.activeUser;
     return (
       <div className="App">
         <Route path='/' component={Header} />
         <Route exact path='/Login' component={Login} />
         <Route exact path='/SignUp' component={SignUp} />
-        <Route exact path='/RentalForm' component={RentalForm} />
-        <Route path='/' component={RentalsContainer} />
+        <Route exact path={`/${userName}/AddRental`} component={RentalForm} />
+        <Route exact path='/' component={RentalsContainer} />
+        <Route exact path={`/${userName}`} component={RentalsContainer} />
+        <Route exact path={`/${userName}/Orders`} component={UserAccountDisplay} />
       </div>
     );
   }
 }
 
+export const mapStateToProps = (state) => ({
+  activeUser: state.activeUser
+});
+
 export const mapDispatchToProps = (dispatch) => ({
-  getLocalStorageUsers: (users) => dispatch(getLocalStorageUsers(users))
+  getLocalStorageUsers: (users) => dispatch(getLocalStorageUsers(users)),
+  getLocalStorageRentals: (rentals) => dispatch(getLocalStorageRentals(rentals))
 });
 
 App.propTypes = {
-  getLocalStorageUsers: PropTypes.func
+  activeUser: PropTypes.object,
+  getLocalStorageUsers: PropTypes.func,
+  getLocalStorageRentals: PropTypes.func
 };
 
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
