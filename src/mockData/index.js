ef6b4e1d59d52861241f4ff7daa7145b30716b34
Joel O'Connor <joconno07@gmail.com>
2018-09-14 10:34:03 -0600

    Mock data built out

---
+++ src/mockData/index.js
@@ -0,0 +1,55 @@
+export const mockUser = {
+  userName: 'Tom Hanks',
+  password: 'password',
+  email: 'THanks@hanks.com',
+  location: '20 McAllister St, San Francisco, CA'
+};
+
+export const mockUsers = [{
+  userName: 'Tom Hanks',
+  password: 'password',
+  email: 'THanks@hanks.com',
+  location: '20 McAllister St, San Francisco, CA'
+},
+{
+  userName: 'Jim Hanks',
+  password: 'password',
+  email: 'JHanks@hanks.com',
+  location: '20 McAllister St, San Francisco, CA'
+}];
+
+export const mockItem = {
+  name: 'Guitar',
+  location: '101 Market St, San Francisco, CA',
+  description: 'Its a guitar!',
+  image: "https://guitar-image.png"
+};
+
+export const mockItems = [{
+  name: 'Guitar',
+  location: '101 Market St, San Francisco, CA',
+  description: 'Its a guitar!',
+  image: "https://guitar-image.png"
+}, 
+{
+  name: 'Amp',
+  location: '101 Market St, San Francisco, CA',
+  description: 'Its a amp!',
+  image: "https://amp-image.png"
+}];
+
+export const mockSenderLocation = '101 Market St, San Francisco, CA';
+export const mockDeliveryLocation = '20 McAllister St, San Francisco, CA';
+
+export const mockQuoteData = {
+  kind: "delivery_quote",
+  fee: 1140,
+  created: "2018-09-13T00:40:02Z",
+  pickup_duration: 5,
+  expires: "2018-09-13T00:45:01Z",
+  currency: "usd",
+  currency_type: "USD",
+  duration: 45,
+  dropoff_eta: "2018-09-13T01:25:02Z",
+  id: "dqt_Ltalsge5ktl1YV"
+};
