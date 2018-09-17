a85e4b17869a1414fc208fcbc9713ae0d2bb93ec
Joel O'Connor <joconno07@gmail.com>
2018-09-16 19:00:02 -0600

    Added selected item into root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,10 +3,12 @@ import { userReducer } from '../reducers/userReducer';
 import { activeUserReducer } from '../reducers/activeUserReducer';
 import { rentalReducer } from '../reducers/rentalReducer';
 import { quoteReducer } from '../reducers/quoteReducer';
+import { selectedItemReducer } from '../reducers/selectedItemReducer';
 
 export const rootReducer = combineReducers({
   users: userReducer,
   activeUser: activeUserReducer,
   rentals: rentalReducer,
-  quote: quoteReducer
+  quote: quoteReducer,
+  selectedItem: selectedItemReducer
 });
