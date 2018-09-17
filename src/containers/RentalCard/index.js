3abf1510147340b5e9c21724e4c9f098fa7cc41e
Joel O'Connor <joconno07@gmail.com>
2018-09-16 18:57:59 -0600

    built out RentalCard to handle selectedItem being put into store, also rental button refactored to only work when user is logged in

--- src/containers/RentalCard/index.js
+++ src/containers/RentalCard/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect, Link } from 'react-router-dom';
 import { getDeliveryQuote } from '../../thunk/getDeliveryQuote';
+import { selectedItem } from '../../actions';
 import PropTypes from 'prop-types';
 import './styles.css';
 
@@ -13,22 +15,35 @@ export class RentalCard extends Component {
   }
 
   handleQuote() {
-    const { userLocation } = this.props.activeUser;
-    const { itemLocation } = this.props;
-    this.props.getDeliveryQuote(userLocation, itemLocation);
+    const { activeUser, rental, selectedItem, getDeliveryQuote } = this.props;
+    selectedItem(rental);
+    getDeliveryQuote(activeUser.userLocation, rental.itemLocation);
+    this.setState({ redirect: true });
   }
 
   render() {
-    const {name, itemLocation, description, image } = this.props;
+    const {name, itemLocation, description, image } = this.props.rental;
+    const { userName } = this.props.activeUser;
+    const rentalButton = () => {
+      return userName ? 
+        <button className='rental-button' onClick={() => this.handleQuote()}>Rent</button> 
+        : <Link to='/Login'><button className='login'>Rent</button></Link>;
+    };
+
     return (
-      <div className='rental-card'>
-        <img className='rental-image' src={image}/>
-        <div className='card-info'>
-          <h1 className='rental-title'>{name}</h1>
-          <h2 className='rental-location'>Location: {itemLocation}</h2>
-          <p className='rental-description'>Info: {description}</p>
-          <button className='rental-button' onClick={() => this.handleQuote()}>Rent</button>
+      <div>
+        <div className='rental-card'>
+          <img className='rental-image' alt='item for rent' src={image}/>
+          <div className='card-info'>
+            <h1 className='rental-title'>{name}</h1>
+            <h2 className='rental-location'>Location: {itemLocation}</h2>
+            <p className='rental-description'>Info: {description}</p>
+            {rentalButton()}
+          </div>
         </div>
+        {this.state.redirect && (
+          <Redirect to={`/${userName}/Confirmation`} />
+        )}
       </div>
     );
   }
@@ -39,18 +54,15 @@ export const mapStateToProps = (state) => ({
 });
 
 export const mapDispatchToProps = (dispatch) => ({
-  getDeliveryQuote: (fromAddress, toAddress) => dispatch(getDeliveryQuote(fromAddress, toAddress))
+  getDeliveryQuote: (fromAddress, toAddress) => dispatch(getDeliveryQuote(fromAddress, toAddress)),
+  selectedItem: (item) => dispatch(selectedItem(item))
 });
 
-// build cards out to have a redirect to a rental confermation if they are logged in
-
 RentalCard.propTypes = {
+  rental: PropTypes.object,
   getDeliveryQuote: PropTypes.func,
   activeUser: PropTypes.object,
-  name: PropTypes.string,
-  itemLocation: PropTypes.string,
-  description: PropTypes.string,
-  image: PropTypes.string
+  selectedItem: PropTypes.func
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(RentalCard);
