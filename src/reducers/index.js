3093395ba6b3b3474181dd554b17f53dad9681dc
Joel O'Connor <joconno07@gmail.com>
2018-09-17 09:40:53 -0600

    Added completed orders into root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,11 +4,13 @@ import { activeUserReducer } from '../reducers/activeUserReducer';
 import { rentalReducer } from '../reducers/rentalReducer';
 import { quoteReducer } from '../reducers/quoteReducer';
 import { selectedItemReducer } from '../reducers/selectedItemReducer';
+import { completeOrdersReducer } from '../reducers/completeOrdersReducer';
 
 export const rootReducer = combineReducers({
   users: userReducer,
   activeUser: activeUserReducer,
   rentals: rentalReducer,
   quote: quoteReducer,
-  selectedItem: selectedItemReducer
+  selectedItem: selectedItemReducer,
+  completedOrders: completeOrdersReducer
 });
