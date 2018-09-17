d058a3ee0abdd2cdc687411a282f1d19369af526
Joel O'Connor <joconno07@gmail.com>
2018-09-16 18:59:01 -0600

    general build for RentalConfirmation to display users quote and to have acess to all data required to place order

--- src/containers/RentalConfirmation/index.js
+++ src/containers/RentalConfirmation/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { Redirect, Link } from 'react-router-dom';
 
 export class RentalConfirmation extends Component {
   constructor() {
@@ -9,21 +11,35 @@ export class RentalConfirmation extends Component {
     };
   }
 
-  render(){
+  render() {
+    const { id, fee, expires, dropoff_eta, duration } = this.props.quote;
     return (
       <div>
-        
+        <h1>Rental Details</h1>
+        <h1>item name</h1>
+        <h1>Estimated Arrival: {dropoff_eta}</h1>
+        <h1>Delivery Duration: {duration}</h1>
+        <h1>Rental Cost: {fee}</h1>
+        <h1>Offer Expires At: {expires}</h1>
+        <button>Accept Order</button>
       </div>
     );
   }
 }
 
+RentalConfirmation.propTypes = {
+  quote: PropTypes.object,
+  activeUser: PropTypes.object
+};
+
 export const mapStateToProps = (state) => ({
-  quote: state.quote
+  quote: state.quote,
+  activeUser: state.activeUser,
+  selectedItem: state.selectedItem
 });
 
 export const mapDispatchToProps = (dispatch) => ({
 
 });
 
-export default connect()(RentalConfirmation);
+export default connect(mapStateToProps)(RentalConfirmation);
