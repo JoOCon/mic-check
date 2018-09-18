c163cc6c36b8ee295245446e2394617a9ccbec66
Joel O'Connor <joconno07@gmail.com>
2018-09-18 09:03:24 -0600

    corrected path name from chnage to Nav

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import NavBar from '../../containers/Nav';
+import NavBar from '../../containers/NavBar';
 import { Route } from 'react-router-dom';
 import './styles.css';
 
