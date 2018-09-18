a58a05cf0a4b56edda7784e75d7d37c70c966d57
Joel O'Connor <joconno07@gmail.com>
2018-09-18 09:29:40 -0600

    Built out App tests for snapshot and MSTP and MDTP

--- src/containers/App/tests/App.test.js
+++ src/containers/App/tests/App.test.js
@@ -1,9 +1,64 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../';
+import { shallow } from 'enzyme';
+import { App, mapStateToProps, mapDispatchToProps } from '../';
+import * as mockData from '../../../mockData';
+import * as actions from '../../../actions';
 
-describe('App Tests', () => {
-  it('should render App with the correct props', () => {
-    expect(true).toEqual(true)
+describe('App tests', () => {
+  let wrapper;
+  let mockRunFunction;
+  const { mockItems, mockUser, mockUsers, mockOrderInfo } = mockData;
+  const { getLocalStorageUsers, getLocalStorageRentals, getLocalStorageOrders } = actions;
+  beforeEach(() => {
+    mockRunFunction = jest.fn();
+    wrapper = shallow( 
+      <App 
+        activeUser={mockUser}
+        getLocalStorageUsers={mockRunFunction}
+        getLocalStorageRentals={mockRunFunction}
+        getLocalStorageOrders={mockRunFunction}
+      /> 
+    );
   });
-})
+
+  it('renders the App with the correct props', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  describe('MapStateToProps', () => {
+    it('should have a activeUser data object in props', () => {
+      const mockState = {activeUser: mockUser};
+      const expected = {activeUser: mockUser};
+      const props = mapStateToProps(mockState);
+      expect(props).toEqual(expected);
+    });
+  });
+
+  describe('MapDispatchToProps', () => {
+    let mockDispatch;
+    beforeEach(() => {
+      mockDispatch = jest.fn();
+    });
+
+    it('should dispatch getLocalStorageUsers when called', () => {
+      const mockAction = getLocalStorageUsers(mockUsers);
+      const props = mapDispatchToProps(mockDispatch);
+      props.getLocalStorageUsers(mockUsers);
+      expect(mockDispatch).toBeCalledWith(mockAction);
+    });
+    
+    it('should dispatch getLocalStorageRentals when called', () => {
+      const mockAction = getLocalStorageRentals(mockItems);
+      const props = mapDispatchToProps(mockDispatch);
+      props.getLocalStorageRentals(mockItems);
+      expect(mockDispatch).toBeCalledWith(mockAction);
+    });
+
+    it('should dispatch getLocalStorageOrders when called', () => {
+      const mockAction = getLocalStorageOrders([mockOrderInfo]);
+      const props = mapDispatchToProps(mockDispatch);
+      props.getLocalStorageOrders([mockOrderInfo]);
+      expect(mockDispatch).toBeCalledWith(mockAction);
+    });
+  });
+});
